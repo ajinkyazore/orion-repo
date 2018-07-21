f544e824be9e5d8fac3abdc1b7cdc4da1304ae4e
ajinkyazore <25245619+ajinkyazore@users.noreply.github.com>
2018-07-22 01:27:03 +0530

    Adding ItemDetail view

--- managementwebapp/src/app/app-routing.module.ts
+++ managementwebapp/src/app/app-routing.module.ts
@@ -1,8 +1,11 @@
 import { NgModule } from '@angular/core';
 import {Routes,RouterModule, Router} from '@angular/router';
 import { DashboardComponent} from './dashboard/dashboard.component';
+import { ItemdetailComponent } from './itemdetail/itemdetail.component';
 
-const routes : Routes = [{path:'dashboard',component:DashboardComponent}];
+const routes : Routes = [
+                            {path:'dashboard',component:DashboardComponent},
+                            {path:'itemDetail',component: ItemdetailComponent}];
 
 @NgModule({
   imports:[RouterModule.forRoot(routes)],
--- managementwebapp/src/app/app.module.ts
+++ managementwebapp/src/app/app.module.ts
@@ -1,20 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './/app-routing.module';
 import { DashboardComponent } from './dashboard/dashboard.component';
-
+import { ItemdetailComponent } from './itemdetail/itemdetail.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { FormsModule} from '@angular/forms';
 
 @NgModule({
   declarations: [
     AppComponent,
-    DashboardComponent
+    DashboardComponent,
+    ItemdetailComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    FormsModule,
+    AppRoutingModule,
+    NgbModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
