c3d34080de246ca598969fe430c9a3b5a8adaa2c
ajinkyazore <25245619+ajinkyazore@users.noreply.github.com>
2018-07-22 01:27:03 +0530

    Adding ItemDetail view

--- managementwebapp/src/app/app.module.ts
+++ managementwebapp/src/app/app.module.ts
@@ -5,12 +5,14 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './/app-routing.module';
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { ItemdetailComponent } from './itemdetail/itemdetail.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    DashboardComponent
+    DashboardComponent,
+    ItemdetailComponent
   ],
   imports: [
     BrowserModule,
